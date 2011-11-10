ac052674e6ddfd0cc87d0f86f2356ab73fa45542
Jeff Schroeder <jeffschroeder@computer.org>
2011-11-09 19:59:39 -0800

    Fix a tyop

--- settings.py
+++ settings.py
@@ -96,7 +96,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(INSTALL_DIR, "/templates"),
+    os.path.join(INSTALL_DIR, "templates"),
 )
 
 INSTALLED_APPS = (
